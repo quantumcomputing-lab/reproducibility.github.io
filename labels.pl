535c38d558bd22e8ee3f33209994a6b7a1c17c3e
Dasgupta, Samudra <dasguptas@ornl.gov>
2022-03-29 17:52:27 -0400

    git restart

---
+++ labels.pl
@@ -0,0 +1,173 @@
+# LaTeX2HTML 2022 (Released January 1, 2022)
+# Associate labels original text with physical files.
+
+
+$key = q/fig:FG_across-time/;
+$external_labels{$key} = "$URL/" . q|node84.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FG_point-in-time/;
+$external_labels{$key} = "$URL/" . q|node84.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FG_raw_lattice/;
+$external_labels{$key} = "$URL/" . q|node84.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FG_raw_time_0/;
+$external_labels{$key} = "$URL/" . q|node84.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FG_raw_time_1/;
+$external_labels{$key} = "$URL/" . q|node84.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FG_raw_time_2/;
+$external_labels{$key} = "$URL/" . q|node84.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FG_raw_time_3/;
+$external_labels{$key} = "$URL/" . q|node84.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FG_raw_time_4/;
+$external_labels{$key} = "$URL/" . q|node84.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FG_raw_time_5/;
+$external_labels{$key} = "$URL/" . q|node84.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FG_raw_time_6/;
+$external_labels{$key} = "$URL/" . q|node84.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FG_raw_time_7/;
+$external_labels{$key} = "$URL/" . q|node84.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FI_across-time/;
+$external_labels{$key} = "$URL/" . q|node83.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FI_point-in-time/;
+$external_labels{$key} = "$URL/" . q|node83.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FI_raw_lattice/;
+$external_labels{$key} = "$URL/" . q|node83.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FI_raw_time_0/;
+$external_labels{$key} = "$URL/" . q|node83.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FI_raw_time_1/;
+$external_labels{$key} = "$URL/" . q|node83.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FI_raw_time_2/;
+$external_labels{$key} = "$URL/" . q|node83.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FI_raw_time_3/;
+$external_labels{$key} = "$URL/" . q|node83.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FI_raw_time_4/;
+$external_labels{$key} = "$URL/" . q|node83.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FI_raw_time_5/;
+$external_labels{$key} = "$URL/" . q|node83.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FI_raw_time_6/;
+$external_labels{$key} = "$URL/" . q|node83.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:FI_raw_time_7/;
+$external_labels{$key} = "$URL/" . q|node83.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:nmi/;
+$external_labels{$key} = "$URL/" . q|node86.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tau_across-time/;
+$external_labels{$key} = "$URL/" . q|node85.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tau_point-in-time/;
+$external_labels{$key} = "$URL/" . q|node85.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tau_raw_time_0/;
+$external_labels{$key} = "$URL/" . q|node85.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tau_raw_time_1/;
+$external_labels{$key} = "$URL/" . q|node85.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tau_raw_time_2/;
+$external_labels{$key} = "$URL/" . q|node85.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tau_raw_time_3/;
+$external_labels{$key} = "$URL/" . q|node85.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tau_raw_time_4/;
+$external_labels{$key} = "$URL/" . q|node85.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tau_raw_time_5/;
+$external_labels{$key} = "$URL/" . q|node85.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tau_raw_time_6/;
+$external_labels{$key} = "$URL/" . q|node85.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tau_raw_time_7/;
+$external_labels{$key} = "$URL/" . q|node85.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:test1/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:test2/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tbl:FG/;
+$external_labels{$key} = "$URL/" . q|node84.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tbl:FI/;
+$external_labels{$key} = "$URL/" . q|node83.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tbl:example/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tbl:nmi/;
+$external_labels{$key} = "$URL/" . q|node86.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tbl:tau/;
+$external_labels{$key} = "$URL/" . q|node85.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2022 (Released January 1, 2022)
+# labels from external_latex_labels array.
+
+
+1;
+
